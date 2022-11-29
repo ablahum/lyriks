6f5c698b9592bf02892dc62fa97fa08b788d1636
ablahum <mriztama@gmail.com>
2022-11-29 23:02:02 +0700

    adding top charts feature

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -11,27 +11,56 @@ import { useGetTopChartsQuery } from '../redux/services';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 
-const TopChartCard = ({ song, idx }) => <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">{song.title}</div>;
+const TopChartCard = ({ song, i, isPlaying, activeSong, handlePause, handlePlay }) => (
+  <div className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}>
+    <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
+
+    <div className="flex-1 flex flex-row justify-between items-center">
+      <img
+        className="w-20 h-20 rounded-lg"
+        src={song?.images?.coverart}
+        alt={song?.title}
+      />
+
+      <div className="flex-1 flex flex-col justify-center mx-3">
+        <Link to={`/songs/${song.key}`}>
+          <p className="text-xl font-bold text-white">{song?.title}</p>
+        </Link>
+
+        <Link to={`/artists/${song?.artists[0].adamid}`}>
+          <p className="text-base text-gray-300 mt-1">{song?.subtitle}</p>
+        </Link>
+      </div>
+    </div>
+
+    <PlayPause
+      isPlaying={isPlaying}
+      activeSong={activeSong}
+      song={song}
+      handlePause={handlePause}
+      handlePlay={handlePlay}
+    />
+  </div>
+);
 
 const TopPlay = () => {
-  const { activeSong, setActiveSong } = useSelector((state) => state.player);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
 
   const dispatch = useDispatch();
   const { data } = useGetTopChartsQuery();
   const divRef = useRef(null);
-
   const topPlays = data?.slice(0, 5);
-
-  const handlePause = () => dispatch(playPause(false));
-
-  const handlePlay = () => {
-    dispatch(setActiveSong({ song, data, idx }));
-    dispatch(playPause(true));
-  };
 
   useEffect(() => {
     divRef.current.scrollIntoView({ behavior: 'smooth' });
   });
+
+  const handlePause = () => dispatch(playPause(false));
+
+  const handlePlay = (song, i) => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
 
   return (
     <div
@@ -52,7 +81,11 @@ const TopPlay = () => {
             <TopChartCard
               key={song.key}
               song={song}
-              idx={i}
+              i={i}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePause={handlePause}
+              handlePlay={() => handlePlay(song, i)}
             />
           ))}
         </div>
