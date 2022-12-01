997d58bfd3e0b549cc2a3b006700705d1c48a6c4
ablahum <mriztama@gmail.com>
2022-12-02 02:06:40 +0700

    fix little bug

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -5,7 +5,14 @@ import { useSelector } from 'react-redux';
 import { Error, Loader, SongCard } from '../components';
 
 const AroundYou = () => {
-  return <div>AroundYou</div>;
+  const [country, setCountry] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+  useEffect(() => {}, [country]);
+
+  return <div></div>;
 };
 
 export default AroundYou;
