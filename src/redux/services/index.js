88e1cbb24f9d475a97c9b94dc7360d6471a05e13
ablahum <mriztama@gmail.com>
2022-12-02 13:34:42 +0700

    adding 'top-artists', 'top charts' features

--- src/redux/services/index.js
+++ src/redux/services/index.js
@@ -12,11 +12,12 @@ export const shazamCore = createApi({
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
+    getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
     getSongDetails: builder.query({ query: ({ songId }) => `/tracks/details?track_id=${songId}` }),
     getSongRelated: builder.query({ query: ({ songId }) => `/tracks/related?track_id=${songId}` }),
     getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
-    getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
   }),
 });
 
-export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongRelatedQuery, useGetArtistDetailsQuery, useGetSongsByCountryQuery } = shazamCore;
+export const { useGetTopChartsQuery, useGetSongsByGenreQuery, useGetSongDetailsQuery, useGetSongRelatedQuery, useGetArtistDetailsQuery, useGetSongsByCountryQuery } = shazamCore;
